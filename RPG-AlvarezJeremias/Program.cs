427077eff8f7bc37e55f58061e4508ef5256d412
AlvarezJeremias <jeremiasal0209@gmail.com>
2022-07-18 20:26:44 -0300

    Juego base terminado

--- RPG-AlvarezJeremias/Program.cs
+++ RPG-AlvarezJeremias/Program.cs
@@ -6,42 +6,247 @@ class Program
 {
     static void Main(string[] args)
     {
-        List <personaje> peleadores=new List<personaje> ();
+        int eleccion=Convert.ToInt32(Console.ReadLine());
+        while (eleccion==1)
+        {
+
+        List <personaje> peleadores=new List<personaje> ();  //se crean las dos listas 
         List<personaje> perdedores = new List<personaje>();
 
         Console.WriteLine("Los dos primeros peleadores seran: ");
-        personaje luchador1=new personaje ();
+        personaje luchador1=new personaje ();// se crean los 4 personajes que participaran
         personaje luchador2=new personaje ();
+        personaje luchador3=new personaje();// en ocasiones, los apodos y nombres de los personajes son iguales, lo que presta mucho a la confusion al momento de ejecutar el programa y leer los registros.
+        personaje luchador4=new personaje ();// Eso queda para solucionarse en un futuro.
         peleadores.Add (luchador1);
-        peleadores.Add (luchador2);
+        peleadores.Add (luchador2);// se añaden los 2 primeros luchadores a la lista de peleadores
+
         foreach (personaje PJ in peleadores)
         {
-            MostrarLuchador(PJ);
+            MostrarLuchador(PJ);  // se muestran los luchadores en la lista actualmente
         }
-         
-            Console.WriteLine("\t----------PELEA:----------");
+        
+        Console.WriteLine("\t----------PELEA:----------");
+        int i;
+            Console.WriteLine($"PELEADOR 1: {peleadores[0].Datos.Apodo}");
+            Console.WriteLine($"PELEADOR 2: {peleadores[1].Datos.Apodo}");
+        for ( i = 0; i < 3; i++)
+        {
 
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine($"\t---RONDA NUMERO {i + 1}---");
-                Console.WriteLine($"ATACANTE: {peleadores[0].Datos.Apodo}");
-                Console.WriteLine($"OPONENTE: {peleadores[1].Datos.Apodo}");
 
-                Pelea(peleadores, 0, 1);
-                i=KO(peleadores,perdedores,i);
-                if (peleadores.Count>1)
+            Pelea(peleadores, 0, 1);   // se realiza el calculo de combate aleatorio
+            i=KO(peleadores,perdedores,luchador1,luchador2,i);  // esta funcion chequea si alguno de los combatientes se quedo sin salud, y devuelve i=5 si asi es, caso contrario devuelve el valor actual de i
+            if (peleadores.Count>1)
             {
                 Console.WriteLine($"{peleadores[1].Datos.Apodo} logro sobrevivir al ataque!");
                 Console.WriteLine($"Ahora, ATACA!");
+                Pelea(peleadores,1,0);
+                i = KO(peleadores, perdedores,luchador2,luchador1, i);
 
             }
 
+        }
+
+        if (i<5) // si ambos jugadores aun tienen vida restante..
+        {
+            if (peleadores[0].Datos.Salud < peleadores[1].Datos.Salud)   // Pierde el que menos salud tenga
+            {
+                perdedores.Add(peleadores[0]);
+                peleadores.RemoveAt(0);
+            }  else
+            {
+                perdedores.Add(peleadores[1]);
+                peleadores.RemoveAt(1);
             }
+        }
         
-         
+        while (peleadores.Count>1)  // Si ocurrio un empate, se pelea hasta que se desempate...
+            {
+                Console.WriteLine("\nHay que desempatar!");
+
+                int k;
+                for (k = 0;  k<3; k++)
+                {
+                Pelea(peleadores, 0, 1);
+                k = KO(peleadores, perdedores,luchador1, luchador2,k);
+                if(peleadores.Count>1)
+                {
+                    Console.WriteLine($"{peleadores[1].Datos.Apodo} logro sobrevivir al ataque!");
+                    Console.WriteLine($"Ahora, ATACA!");
+                    Pelea(peleadores, 1, 0);
+                    k= KO(peleadores, perdedores, luchador2, luchador1, k);
+                }
+                }
+                if (k < 5) // si ambos jugadores aun tienen vida restante..
+                {
+                    if (peleadores[0].Datos.Salud < peleadores[1].Datos.Salud)   // Pierde el que menos salud tenga
+                    {
+                        perdedores.Add(peleadores[0]);
+                        peleadores.RemoveAt(0);
+                    }
+                    else
+                    {
+                        perdedores.Add(peleadores[1]);
+                        peleadores.RemoveAt(1);
+                    }
+                }
+
+
+            }
+        int j;
+        if (peleadores.Count == 1)
+        {
+            Console.WriteLine("\nA continuacion, pelearan otros dos competidores!");
+            peleadores.Add(luchador3);
+            peleadores.Add(luchador4);
+            MostrarLuchador(luchador3);
+            MostrarLuchador(luchador4);
+
+            Console.WriteLine("\nAhora: A pelear!");
+
+                Console.WriteLine($"PELEADOR 3: {peleadores[1].Datos.Apodo}");
+                Console.WriteLine($"PELEADOR 4: {peleadores[2].Datos.Apodo}");
+            for (j = 0; j < 3; j++)
+            {
+
+                Pelea(peleadores, 1, 2);
+                i = KO(peleadores, perdedores, luchador3, luchador4, j);
+                if (peleadores.Count > 1)
+                {
+
+                    Pelea(peleadores, 2, 1);
+                    j = KO(peleadores, perdedores, luchador4, luchador3, j);
+
+
+                }
+            }
+            if (j < 5)
+            {
+                if (luchador3.Datos.Salud < luchador4.Datos.Salud)
+                {
+                    perdedores.Add(luchador3);
+                    peleadores.Remove(luchador3);
+                }
+                else
+                {
+                    perdedores.Add(luchador4);
+                    peleadores.Remove(luchador4);
+                }
+            }
+                while (peleadores.Count > 2)  // Si quedan mas de 2 jugadores en la tabla, se siguen peleando los luchadores 3 y 4 hasta que desempaten.
+                {
+                    Console.WriteLine("\nHay que desempatar!");
+
+                    int x;
+                    for (x = 0; x < 3; x++)
+                    {
+                        Pelea(peleadores, 1, 2);
+                        x = KO(peleadores, perdedores, luchador3, luchador4, x);
+                        if (peleadores.Count > 2)
+                        {
+
+                            Pelea(peleadores, 2, 1);
+                            x = KO(peleadores, perdedores, luchador4, luchador3, x);
+                        }
+                    }
+                    if (x < 5) // si ambos jugadores aun tienen vida restante..
+                    {
+                        if (luchador3.Datos.Salud < luchador4.Datos.Salud)   // Pierde el que menos salud tenga
+                        {
+                            perdedores.Add(luchador3);
+                            peleadores.Remove(luchador3);
+                        }
+                        else
+                        {
+                            perdedores.Add(luchador4);
+                            peleadores.Remove(luchador4);
+                        }
+                    }
+
+
+                }
+            }
+            Console.WriteLine("\nsolamente quedan los finalistas!  ellos son: ");
+            Console.WriteLine($"\n\n {peleadores[0].Datos.Nombre} y {peleadores[1].Datos.Nombre}!");
+
+            Console.WriteLine("Ahora, se pelearan por el trono de hierro! :");
+            Console.WriteLine("\nAhora: A pelear!");
+
+            Console.WriteLine($"FINALISTA 1: {peleadores[0].Datos.Apodo}");
+            Console.WriteLine($"FINALISTA 2: {peleadores[1].Datos.Apodo}");
+            int f;
+            for (f = 0;  f< 3; f++)
+            {
+
+                Pelea(peleadores, 0, 1);
+                f = KO(peleadores, perdedores, peleadores[0], peleadores[1], f);
+                if (peleadores.Count > 1)
+                {
+
+                    Pelea(peleadores, 1, 0);
+                    f = KO(peleadores, perdedores, peleadores[1], peleadores[0], f);
+
+
+                }
+            }
+            if (f< 5)
+            {
+                if (peleadores[0].Datos.Salud < peleadores[1].Datos.Salud)
+                {
+                    perdedores.Add(peleadores[0]);
+                    peleadores.RemoveAt(0);
+                }
+                else
+                {
+                    perdedores.Add(peleadores[1]);
+                    peleadores.RemoveAt(1);
+                }
+            }
+            while (peleadores.Count > 1)  // Si quedan mas de 2 jugadores en la tabla, se siguen peleando los luchadores hasta que desempaten.
+            {
+                Console.WriteLine("\nHay que desempatar!");
+
+                int x;
+                for (x = 0; x < 3; x++)
+                {
+                    Pelea(peleadores, 0, 1);
+                    x = KO(peleadores, perdedores, peleadores[0], peleadores[1], x);
+                    if (peleadores.Count > 1)
+                    {
+
+                        Pelea(peleadores, 1, 0);
+                        x = KO(peleadores, perdedores, peleadores[1], peleadores[0], x);
+                    }
+                }
+                if (x < 5) // si ambos jugadores aun tienen vida restante..
+                {
+                    if (peleadores[0].Datos.Salud < peleadores[1].Datos.Salud)   // Pierde el que menos salud tenga
+                    {
+                        perdedores.Add(peleadores[0]);
+                        peleadores.RemoveAt(0);
+                    }
+                    else
+                    {
+                        perdedores.Add(peleadores[1]);
+                        peleadores.RemoveAt(1);
+                    }
+                }
+
+
+            }
+            foreach (personaje PJ in peleadores)
+            {
+                Console.WriteLine($"\nQuedo el finalista: {PJ.Datos.Nombre}");
+            }
+            eleccion = Convert.ToInt32(Console.ReadLine());
+        }
 
     }
+/*COMENTARIOS PARA EL MEJORAMIENTO PROXIMO DEL JUEGO
+ -el proceso de If's,While's y for's necesarios para llevar a cabo las peleas se utiliza mucho, se tendra muy en cuenta convertirlo en una funcion para mejorar el aspecto del codigo.
+-Actualmente decidí hacer el RPG solo con 4 luchadores, pero solo seria necesario repetir el mismo codigo indefinidamente para que sea posible hacerlo con mas jugadores
 
+ */
    public static void MostrarLuchador(personaje PJ)
     {
         Console.WriteLine("----------------------------------------------------------");
@@ -95,21 +300,53 @@ class Program
 
     }
 
-    public static int KO (List<personaje> listaPeleadores, List<personaje>listaPerdedores, int opcion)
+    public static int KO (List<personaje> listaPeleadores, List<personaje>listaPerdedores, personaje PJ1, personaje PJ2, int opcion)
     {
-        if (listaPeleadores[0].Datos.Salud<=0){
-            listaPerdedores.Add(listaPeleadores[0]);
-            listaPeleadores.RemoveAt(0);
+        if (PJ1.Datos.Salud<=0){
+            listaPerdedores.Add(PJ1);
+            listaPeleadores.Remove(PJ1);
             return 5;
         }else
         {
-            if (listaPeleadores[1].Datos.Salud<=0)
+            if (PJ2.Datos.Salud<=0)
             {
-                listaPerdedores.Add(listaPeleadores[1]);
-                listaPeleadores.RemoveAt(1);
+                listaPerdedores.Add(PJ2);
+                listaPeleadores.Remove(PJ2);
                 return 5;
             }
         }
         return opcion;
     }
+    public static void  AumentoCaracteristicas (personaje PJ)
+    {
+        int opcion=0;
+        while (opcion != 1 || opcion!=2 || opcion !=3) 
+        {
+            Console.WriteLine("El ganador sera premiado con un bonus de una caracteristica a eleccion!");
+            Console.WriteLine("Elija cual sera la bonificacion que se llevara a cabo: \nOpcion 1: +10 Salud\nOpcion 2: +2 Fuerza.\nOpcion 3: +2 Armadura. ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+ ;           switch (opcion)
+            {
+                case 1:
+                    PJ.Datos.Salud += 10;
+                    Console.WriteLine("Se a aumentado la salud del personaje en 10 puntos.");
+                    Console.WriteLine($"Salud Actual: {PJ.Datos.Salud} puntos.");
+                    break;
+
+                case 2:
+                    PJ.Caracteristicas.Fuerza += 2;
+                    Console.WriteLine("Se a aumentado la fuerza del personaje en 2 puntos.");
+                    Console.WriteLine($"Fuerza Actual: {PJ.Caracteristicas.Fuerza} puntos.");
+                    break;
+                case 3:
+                    PJ.Caracteristicas.Armadura += 2;
+                    Console.WriteLine("Se a aumentado la armadura del personaje en 2 puntos.");
+                    Console.WriteLine($"Armadura Actual: {PJ.Caracteristicas.Armadura} puntos.");
+                    break;
+            }
+        }
+
+
+    }
+
 }
